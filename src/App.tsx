b681ac9cc3157394fd444cbf27dee771801b34df
Filippo-Dembech <Filippo.dembech@gmail.com>
2026-06-24 13:06:33 +0200

    core > deploy app on Github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Ruolo from './pages/Ruolo'
 import RuoloDetail from './pages/RuoloDetail'
